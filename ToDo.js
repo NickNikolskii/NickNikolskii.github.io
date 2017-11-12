fef24d4f38a0548561dfbdfdf828338fdddfe9ed
NickNikolskii <kolyanikolsky@gmail.com>
2017-11-12 16:36:33 +0300

    Update ToDo.js

--- ToDo.js
+++ ToDo.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $('.wrapper > div > button').click(function () {
+    $('.wrapper > div > button(value = "add")').click(function () {
         var a = $('input').val();
         $('.item_list').append('<div id="a" class="item">' + a + '</div>' + <button value = "remove"> + <"Udalit'"> + <button>);
         
