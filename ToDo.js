31b740155b4fbf385845edc252ea14d17b6ddfe3
NickNikolskii <kolyanikolsky@gmail.com>
2017-11-12 16:46:11 +0300

    Update ToDo.js

--- ToDo.js
+++ ToDo.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
 
     $('.wrapper > div > button').click(function () {
         var a = $('input').val();
-        $('.item_list').append('<div id="test" class="item">' + a + '</div>');
+        $('.item_list').append('<div class="item">' + a + '</div>');
         $('input').val("");
     });
 
-    $("#test").dblclick(function () {
-        alert("!!!");
+    $("item").dblclick(function () {
+        alert("Udalis' uje");
         $(this).toggleClass("line-through");
     });
 
